32737b684b171c1446679a0a158e550b43e2b386
zbz <nnzbz@21cn.com>
2024-04-25 10:45:34 +0800

    + 添加restTemplate的bean，并支持解析gzip的响应

---
+++ src/main/java/magicapi/config/RestTemplateConfig.java
@@ -0,0 +1,32 @@
+package magicapi.config;
+
+
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration(proxyBeanMethods = false)
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        RestTemplate restTemplate = new RestTemplate(factory);
+        // 支持中文编码
+        restTemplate.getMessageConverters().set(1,
+                new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
+
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().build());
+        return factory;
+    }
+}
