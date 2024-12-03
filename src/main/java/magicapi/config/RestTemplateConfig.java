ae338d79fb95dac77be635e9db9495798852b73c
lwc <lan1069135464@163.com>
2024-12-03 11:30:36 +0800

    * 自定义http模块配置

--- src/main/java/magicapi/config/RestTemplateConfig.java
+++ src/main/java/magicapi/config/RestTemplateConfig.java
@@ -1,18 +1,56 @@
 package magicapi.config;
 
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.ssssssss.magicapi.modules.http.HttpModule;
 
+import javax.net.ssl.SSLContext;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration(proxyBeanMethods = false)
 public class RestTemplateConfig {
+
+    @Bean
+    public SSLContext sslContext() {
+        try {
+            TrustStrategy acceptingTrustStrategy = (chain, authType) -> true;
+            return SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        } catch (KeyStoreException | KeyManagementException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public CloseableHttpClient closeableHttpClient(SSLContext sslContext) {
+        SSLConnectionSocketFactory ssl           = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+        HttpClientBuilder          clientBuilder = HttpClients.custom();
+        return clientBuilder.setConnectionManager(PoolingHttpClientConnectionManagerBuilder
+                .create().setSSLSocketFactory(ssl).build()).build();
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory(CloseableHttpClient closeableHttpClient) {
+//        HttpComponentsClientHttpRequestFactory factory =
+//                new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
+        return new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
+    }
+
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         RestTemplate restTemplate = new RestTemplate(factory);
@@ -21,10 +59,13 @@ public class RestTemplateConfig {
         return restTemplate;
     }
 
+    /**
+     * magic-api的 http模块封装，加入自定义RestTemplate
+     * @param restTemplate
+     * @return
+     */
     @Bean
-    public ClientHttpRequestFactory clientHttpRequestFactory() {
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
-        return factory;
+    public HttpModule magicHttpModule(RestTemplate restTemplate) {
+        return new HttpModule(restTemplate);
     }
 }
