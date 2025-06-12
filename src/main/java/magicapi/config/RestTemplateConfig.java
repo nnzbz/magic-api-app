5f8a1b5ce2bb9bb62a4c9e3fbc28b0b58a637d65
zbz <nnzbz@qq.com>
2025-06-12 08:57:10 +0800

    * 合并自定义 http 支持 https

--- src/main/java/magicapi/config/RestTemplateConfig.java
+++ src/main/java/magicapi/config/RestTemplateConfig.java
@@ -1,15 +1,27 @@
 package magicapi.config;
 
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.ssssssss.magicapi.modules.http.HttpModule;
 
+import javax.net.ssl.SSLContext;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration(proxyBeanMethods = false)
 public class RestTemplateConfig {
@@ -34,8 +46,6 @@ public class RestTemplateConfig {
 
     @Bean
     public ClientHttpRequestFactory clientHttpRequestFactory(CloseableHttpClient closeableHttpClient) {
-//        HttpComponentsClientHttpRequestFactory factory =
-//                new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
         return new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
     }
 
@@ -49,8 +59,6 @@ public class RestTemplateConfig {
 
     /**
      * magic-api的 http模块封装，加入自定义RestTemplate
-     * @param restTemplate
-     * @return
      */
     @Bean
     public HttpModule magicHttpModule(RestTemplate restTemplate) {
